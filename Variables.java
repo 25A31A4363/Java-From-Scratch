548f195f19de0ee3443b1f19462e680c212ffd3b
Mohith Sripathi <sripathi.mohith0408@gmail.com>
2026-07-21 22:53:02 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,20 @@
+public class Variables{
+	public static void main(String[] args){
+		String name = "Mohith";
+		int age = 17;
+		String branch = "CSE-AI";
+		char section = 'A';
+		String rollno = "25A31A4363";
+
+		System.out.println("Name: "+name);
+		System.out.println("Age: "+age);
+		System.out.println("Branch: "+branch);
+		System.out.println("Section: "+section);
+		System.out.println("Roll Number: "+rollno);
+	}
+}
+
+		
+
+		
+		
