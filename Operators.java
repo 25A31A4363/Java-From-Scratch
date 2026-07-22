09ee64fe25f52d4686c827d6b0512457d572cae7
Mohith Sripathi <sripathi.mohith0408@gmail.com>
2026-07-22 23:12:35 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,11 @@
+class Operators{
+	public static void main(String[] args){
+		float a = 5;
+		float b = 2;
+	System.out.println("Addition: "+(a+b));
+	System.out.println("Subtraction: "+(a-b));
+	System.out.println("Multiplication: "+(a*b));
+	System.out.println("Division: "+(a/b));
+	System.out.println("Remainder: "+(a%b));
+  }
+}
