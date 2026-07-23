4010ae9c05d2eca70f9470c32851c28c0758a0bc
Mohith Sripathi <sripathi.mohith0408@gmail.com>
2026-07-23 22:10:07 +0530

    Add files via upload

---
+++ EvenOROdd.java
@@ -0,0 +1,10 @@
+public class EvenOROdd{
+	public static void main(String[] args){
+		int a = 5;
+		if(a%2 == 0){
+		 System.out.println("Given Number is Even");
+		}else{
+		 System.out.println("Given Number is Odd");
+		}
+	}
+}
