bcc2043602829e2ca78032239f7c4fa170c7db90
Mohith Sripathi <sripathi.mohith0408@gmail.com>
2026-07-23 22:15:37 +0530

    Add files via upload

---
+++ ConditionalStatements.java
@@ -0,0 +1,14 @@
+class ConditionalStatements{
+	public static void main(String[] args){
+		int marks = 89;
+	if(marks >=90){
+	  System.out.println("A Grade");
+	}else if(marks >= 75){
+	  System.out.println("B Grade");
+	}else if(marks >= 50){
+	  System.out.println("C Grade");
+	}else{
+	  System.out.println("Fail");
+	}
+    }
+}
