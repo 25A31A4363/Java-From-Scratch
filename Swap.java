174d90862f57ab650ba037d54bcbf166ec5d8015
Mohith Sripathi <sripathi.mohith0408@gmail.com>
2026-07-25 22:33:33 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,11 @@
+class Swap{
+	public static void main(String[] args){
+		int a = 5;
+		int b = 10;
+	System.out.println("Before Swapping: a = "+ a +" and b = "+b);
+	  int t = a;
+	  a = b;
+	  b = t;
+	System.out.println("After Swapping: a = "+ a +" and b = "+b);
+ 	}
+}
