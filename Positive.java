a89f8160038d265ce50b09e3378f0f8d78d0d16b
Mohith Sripathi <sripathi.mohith0408@gmail.com>
2026-07-28 22:28:00 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Positive{
+	public static void main(String[] args){
+	    Scanner sc = new Scanner(System.in);
+	    int n;
+	    System.out.println("Enter a Number:");
+	    n = sc.nextInt();
+
+	    if(n>0){
+		System.out.println("Positive Number");
+	    }
+	}
+   }
