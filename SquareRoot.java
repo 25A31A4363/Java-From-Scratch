52246a7a518ede67572e66ad92c85d11ad0d1c82
Mohith Sripathi <sripathi.mohith0408@gmail.com>
2026-07-29 22:27:03 +0530

    Add files via upload

---
+++ SquareRoot.java
@@ -0,0 +1,7 @@
+class SquareRoot {     
+	public static void main(String args[]){         
+	int n = 49;          
+	double root = Math.sqrt(n);         
+        System.out.println("Square Root = " + root);     
+	} 
+} 
