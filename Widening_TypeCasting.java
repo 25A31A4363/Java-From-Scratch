7c189b9fba2faece0917b727a118386609618344
Mohith Sripathi <sripathi.mohith0408@gmail.com>
2026-08-05 23:48:08 +0530

    Add Widening_TypeCasting class with main method

---
+++ Widening_TypeCasting.java
@@ -0,0 +1,9 @@
+class Widening_TypeCasting{
+  public static void main(String[] args){
+  int num = 9;
+  double Num = myInt; // Automatic casting: int to double
+
+  System.out.println(num);    // Outputs 9
+  System.out.println(Num); // Outputs 9.0
+      }
+}
