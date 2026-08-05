2387989dba14ce4ac2eaa753f3fefe94d5363744
Mohith Sripathi <sripathi.mohith0408@gmail.com>
2026-08-05 23:51:05 +0530

    Implement manual type casting from double to int
    Added manual casting from double to int in main method.

---
+++ Narrowing_TypeCasting.java
@@ -0,0 +1,9 @@
+class Narrowing_TypeCasting{
+  public static void main(String[] args){
+      double decimal = 9.78d;
+int num = (int) myDouble; // Manual casting: double to int
+
+System.out.println(decimal); // Outputs 9.78
+System.out.println(num);    // Outputs 9
+    }
+}
