dbf0f44dde178469aa1cba87a3f1cbe46b7405d5
Mohith Sripathi <sripathi.mohith0408@gmail.com>
2026-09-01 23:15:17 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,10 @@
+class Student{
+    Student(){
+        System.out.println("Constructor created");
+    }
+}
+public class Constructor{
+    public static void main(String[] args){
+        Student s1 = new Student();
+    }
+}
